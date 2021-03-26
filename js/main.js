4b71d88936fbbd611d02b06de4a4d63a7f28bdc7
yarbest <72338354+yarbest@users.noreply.github.com>
2021-03-26 15:07:05 +0200

    1

--- js/main.js
+++ js/main.js
@@ -1,41 +1,74 @@
-const mySwiper = new Swiper(".swiper-container", {
+const mySwiper = new Swiper('.swiper-container', {
     loop: true,
 
     // Navigation arrows
     navigation: {
-        nextEl: ".slider-button-next",
-        prevEl: ".slider-button-prev",
+        nextEl: '.slider-button-next',
+        prevEl: '.slider-button-prev',
     },
 });
 
 //Cart
 (function () {
-    const buttonCart = document.querySelector(".button-cart");
-    const modalCart = document.querySelector("#modal-cart");
+    const buttonCart = document.querySelector('.button-cart');
+    const modalCart = document.querySelector('#modal-cart');
 
-    const openModal = () => modalCart.classList.add("show");
+    const openModal = () => modalCart.classList.add('show');
 
-    const closeModal = () => modalCart.classList.remove("show");
+    const closeModal = () => modalCart.classList.remove('show');
 
-    buttonCart.addEventListener("click", openModal);
+    buttonCart.addEventListener('click', openModal);
 
-    modalCart.addEventListener("click", (event) => {
+    modalCart.addEventListener('click', (event) => {
         const target = event.target;
-        if (target.matches("#modal-cart, .modal-close")) closeModal();
+        if (target.matches('#modal-cart, .modal-close')) closeModal();
     });
 })();
 
 //smooth scroll
 (function () {
-    const scrollLinks = document.querySelectorAll("a.scroll-link");
+    const scrollLinks = document.querySelectorAll('a[href*="#"]');
+
+    const scrollToBlock = (event, link) => {
+        event.preventDefault();
+        let currentBlock = document.querySelector(link.getAttribute('href')); // elem.getAttribute('href') вернет нам id того блока, на который ведет эта ссылка
+        let currentWindowScroll = window.pageYOffset; //начинаем прокручивать страницу из текущего значения прокрутки, чтобы не телепортироваться
+        let scrollHeigthPerMove = Math.abs(currentWindowScroll - currentBlock.offsetTop) > document.documentElement.clientHeight ? 60 : 30; //если блок не видно на текущем окне, то нужно быстро прокручивать
+        //Если в данный момент окно прокручено дальше, чем располагается нужный блок, то нужно прокручивать на верх, уменьшая значение currentWindowScroll
+        if (currentWindowScroll >= currentBlock.offsetTop) {
+            const scrollAbove = () => {
+                currentWindowScroll -= scrollHeigthPerMove;
+                if (currentWindowScroll <= currentBlock.offsetTop) {
+                    //если окно прокрутили выше чем нужный блок, то останавливаемся и устанавливаем окно пользователя на уровне с верхушкой нужного блока
+                    document.documentElement.scrollTo(0, currentBlock.offsetTop);
+                    return;
+                }
+                document.documentElement.scrollTo(0, currentWindowScroll); //потихоньку прокручиваем окно
+                requestAnimationFrame(scrollAbove);
+            };
+
+            requestAnimationFrame(scrollAbove);
+        }
+        //Если в данный момент окно находится выше, чем располагается нужный блок то нужно прокручивать вниз увеличивая значение currentWindowScroll
+        if (currentWindowScroll <= currentBlock.offsetTop) {
+            const scrollBellow = () => {
+                currentWindowScroll += scrollHeigthPerMove;
+                if (currentWindowScroll >= currentBlock.offsetTop) {
+                    //если окно прокрутили ниже чем блок, то останавливаемся и устанавливаем окно пользователя на уровне с верхушкой нужного блока
+                    document.documentElement.scrollTo(0, currentBlock.offsetTop); //эта строчка обязательно нужна, так как окно нужно поставить ровно с блоком, иначе может быть сдвиг
+                    return;
+                }
+
+                document.documentElement.scrollTo(0, currentWindowScroll); //потихоньку прокручиваем окно
+                requestAnimationFrame(scrollBellow);
+            };
+            requestAnimationFrame(scrollBellow);
+        }
+    };
+
     scrollLinks.forEach((link) => {
-        link.addEventListener("click", (event) => {
-            event.preventDefault();
-            const blockId = link.getAttribute("href");
-            document.querySelector(blockId).scrollIntoView({
-                behavior: "smooth",
-                block: "center",
-            });
+        link.addEventListener('click', (event) => {
+            scrollToBlock(event, link);
         });
     });
 })();
